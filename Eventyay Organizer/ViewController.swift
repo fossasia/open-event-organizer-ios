e4488ec0b70ae91ae4819b12b082b501f7b7976b
Jogendra Singh <imjog24@gmail.com>
2018-11-25 19:44:41 +0530

    initial project setup

---
+++ Eventyay Organizer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Eventyay Organizer
+//
+//  Created by JOGENDRA on 25/11/18.
+//  Copyright © 2018 FOSSAsia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
