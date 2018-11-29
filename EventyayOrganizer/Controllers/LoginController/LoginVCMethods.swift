ce56ab8d949311b1ec4edc8aada93c24c132950b
Jogendra Singh <imjog24@gmail.com>
2018-11-29 16:07:36 +0530

    Structured project style

---
+++ EventyayOrganizer/Controllers/LoginController/LoginVCMethods.swift
@@ -0,0 +1,13 @@
+//
+//  LoginVCMethods.swift
+//  EventyayOrganizer
+//
+//  Created by JOGENDRA on 29/11/18.
+//  Copyright © 2018 FOSSAsia. All rights reserved.
+//
+
+import UIKit
+
+extension LoginViewController {
+
+}
---
+++ EventyayOrganizer/Controllers/LoginController/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  EventyayOrganizer
+//
+//  Created by JOGENDRA on 29/11/18.
+//  Copyright © 2018 FOSSAsia. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
