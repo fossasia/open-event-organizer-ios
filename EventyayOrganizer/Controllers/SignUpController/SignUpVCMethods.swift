b2707f96f4e8abdfa668bc04a7728b858930568e
Dilum De Silva <dilumdesilva.info@gmail.com>
2019-04-07 05:54:24 +0530

    feat: Getting started screen with user account validation via email
    Implemented the getting started screen which asks the user for the email to validate whether the
    user already has an account or not.
    if the user has an account already the starting screen will navigate directly to the login screen
    after the email validation. if the user doesn't have an account it will redirect user to the signup screen.
    
    Implemented #20
    
    fix: Moved constant values to the ControllerConstants.swift file.

--- EventyayOrganizer/Controllers/SignUpController/SignUpVCMethods.swift
+++ EventyayOrganizer/Controllers/SignUpController/SignUpVCMethods.swift
@@ -53,20 +53,25 @@ extension SignUpViewController {
         addressTextField.placeholderActiveColor = .defaultColor()
         addressTextField.dividerNormalColor = .iOSGray()
         addressTextField.dividerActiveColor = .defaultColor()
+        addressTextField.text = ControllerConstants.CommonURL.Debug.baseURL
         addressTextField.textColor = .black
+    }
+    
+    func prepareToggleRadioButton(){
+        personalServerButton.checkState = .checked
     }
 
     @IBAction func toggleRadioButtons(_ sender: M13Checkbox) {
         if sender.checkState == .checked {
             addressTextField.tag = 1
             addressTextField.isUserInteractionEnabled = false
-            addressTextField.text = "https://api.eventyay.com/v1/"
+            addressTextField.text = ControllerConstants.CommonURL.Debug.baseURL
             addressTextField.placeholder = ""
         } else {
             addressTextField.tag = 0
             addressTextField.isUserInteractionEnabled = true
             addressTextField.text = ""
-            addressTextField.placeholder = "Custom Server URL"
+            addressTextField.placeholder = ControllerConstants.Placeholders.customServerURL
         }
     }
 
--- Podfile
+++ Podfile
@@ -10,6 +10,9 @@ target 'EventyayOrganizer' do
   pod 'Motion', :git => 'https://github.com/CosmicMind/Motion.git', :branch => 'development'
   pod 'Material', :git => 'https://github.com/CosmicMind/Material.git', :branch => 'development'
   pod 'M13Checkbox', :git => 'https://github.com/Marxon13/M13Checkbox.git', :branch => 'swift_4_2'
+  pod 'ObjectMapper', '~> 3.4'
+  pod 'AlamofireObjectMapper', '~> 5.2'
+  pod 'NVActivityIndicatorView'
   shared_pods
 end
 
